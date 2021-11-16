517006625e987d00515659fa76fba9de98ffa746
Jared B <jburnt17@gmail.com>
2021-11-15 20:32:34 -0500

    added sign up page functionality

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,8 +2,24 @@ window.addEventListener("DOMContentLoaded", e =>{
     const signUpButton = document.querySelector('.sign-up');
     const signUpForm = document.querySelector('.sign-up-form');
     const signInForm = document.querySelector('.form-container');
+    const closeForm = document.querySelector('.fa-xmark')
+
+    const out = e => {
+        if (e.target == document.body) {
+            signUpForm.style.display = 'none';
+            signInForm.style.display = 'flex';
+        };
+    };
+
+    window.addEventListener('click', out);
+
     signUpButton.addEventListener('click', e => {
         signUpForm.style.display = 'flex';
         signInForm.style.display = 'none';
+    });
+
+    closeForm.addEventListener('click', e => {
+        signUpForm.style.display = 'none';
+        signInForm.style.display = 'flex';
     });
 })
