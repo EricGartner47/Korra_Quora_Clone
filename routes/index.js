f7f98ce54ac29e684ed8a0597ff62af03c3785a9
Jared B <jburnt17@gmail.com>
2021-11-16 11:13:39 -0500

    fixed footer text

--- routes/index.js
+++ routes/index.js
@@ -4,19 +4,30 @@ const { csrfProtection, asyncHandler } = require('./utils')
 const db = require('../db/models')
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../auth')
-const { bcrypt } = require('bcryptjs')
+const { bcrypt } = require('bcryptjs');
+const { isTemplateLiteral } = require('babel-types');
+
 /* GET home page. */
 router.get('/', csrfProtection, function (req, res, next) {
   res.render('home', { csrfToken: req.csrfToken() });
 });
 
 const loginValidations = [
+  check("username")
+    .exists({checkFalsy: true})
+    .withMessage('Please provide a value for username')
+    .isLength({max: 55})
+    .withMessage('Username must not be more than 55 characters long'),
   check("email")
     .exists({ checkFalsy: true })
-    .withMessage("Please provide a valid email."),
+    .withMessage("Please provide a valid email")
+    .isLength({max: 55})
+    .withMessage('Email address must not be more than 55 characters long')
+    .isEmail()
+    .withMessage('Email Address is not valid email'),
   check("password")
     .exists({ checkFalsy: true })
-    .withMessage("Please provide a valid password."),
+    .withMessage("Please provide a valid password"),
 ]
 
 router.post('/login', csrfProtection, loginValidations, handleValidationErrors, asyncHandler(async (req, res) => {
