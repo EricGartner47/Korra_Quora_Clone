23ca720c095f70cfd3a6eec283a0d9f99d0bb124
Jared B <jburnt17@gmail.com>
2021-11-15 19:33:58 -0500

    added semi-functional sign-up

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,9 @@
-window.addEventListener("load", (event)=>{
-    console.log("hello from javascript!")
+window.addEventListener("DOMContentLoaded", e =>{
+    const signUpButton = document.querySelector('.sign-up');
+    const signUpForm = document.querySelector('.sign-up-form');
+    const signInForm = document.querySelector('.form-container');
+    signUpButton.addEventListener('click', e => {
+        signUpForm.style.display = 'flex';
+        signInForm.style.display = 'none';
+    });
 })
