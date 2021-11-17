b4a59d6c3d4a0166f6de20bfd9d45e2a971adaaf
Your Name <email@example.com>
2021-11-17 16:22:40 -0500

    question errors and add question button render

--- routes/questions.js
+++ routes/questions.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { csrfProtection, asyncHandler } = require('./utils')
+const {check, validationResult} = require('express-validator')
 const db = require('../db/models')
 
 /* GET users listing. */
@@ -17,26 +18,39 @@ router.get('/create',csrfProtection, asyncHandler(async(req, res, next) => {
 
 //Question Validators for adding a new question
 const questionValidators = [
-  check('question')
+  check('topic')
     .exists({ checkFalsy: true })
-    .withMessage('Please provide a value for question field'),
+    .withMessage('Please select a topic for your question'),
   check('title')
     .exists({ checkFalsy: true })
-    .withMessage('Please provide a value for title field')
+    .withMessage('Please provide a title for your question')
     .isLength({ max: 50 })
     .withMessage('Title must not be more than 50 characters long'),
+  check('description')
+    .exists({checkFalsy: true})
+    .withMessage('Please provide a description for your question')
+    .isLength({max: 2000})
+    .withMessage('Question description must not be greater than 2000 characters')
 ];
 
 router.post('/create', csrfProtection, questionValidators, asyncHandler(async(req, res) => {
   const user = await db.User.findByPk(req.session.auth.userId);
-
-  let { title, description, userId, topicId} = req.body;
-  const newQuestion = Question.build({
-    title,
-    description,
-    userId,
-    topicId
-  });
+  const topics = await db.Topic.findAll()
+  let { title, description, topic} = req.body;
+  const validatorErrors = validationResult(req);
+    if(validatorErrors.isEmpty()){
+      const newQuestion = await db.Question.build({
+        title,
+        description,
+        topicId: topic,
+        userId: user.id
+      });
+      await newQuestion.save()
+      res.redirect('/questions')
+    } else {
+      const errors = validatorErrors.array().map((error) => error.msg);
+      res.render('add-question', {errors, topics, csrfToken: req.csrfToken()})
+    }
 }))
 
 module.exports = router;
