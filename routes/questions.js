0005bc3ebbf2cff2a8d04a2cf2350a445dcf3277
depash <depash2003@gmail.com>
2021-11-17 19:06:13 -0500

    finished delete and edit questions

--- routes/questions.js
+++ routes/questions.js
@@ -33,7 +33,7 @@ const questionValidators = [
   check('title')
     .exists({ checkFalsy: true })
     .withMessage('Please provide a title for your question')
-    .isLength({ max: 500})
+    .isLength({ max: 500 })
     .withMessage('Title must not be more than 500 characters long'),
   check('description')
     .exists({ checkFalsy: true })
@@ -62,10 +62,40 @@ router.post('/create', csrfProtection, questionValidators, asyncHandler(async (r
   }
 }))
 
-router.get('/:id/delete', asyncHandler(async(req, res)=>{
+router.get('/:id/delete', asyncHandler(async (req, res) => {
   const deleteQuestion = await db.Question.findByPk(req.params.id)
   await deleteQuestion.destroy()
   res.redirect('/questions');
 }))
 
+router.get('/:id/edit', csrfProtection, questionValidators, asyncHandler(async (req, res) => {
+  const topics = await db.Topic.findAll()
+  const question = await db.Question.findByPk(req.params.id, {
+    include: [{
+      model: db.Topic
+    }]
+  })
+  res.render('question-edit', { topics, csrfToken: req.csrfToken(), question });
+}))
+
+router.post('/:id/edit', csrfProtection, questionValidators, asyncHandler(async (req, res) => {
+  const user = await db.User.findByPk(req.session.auth.userId);
+  const topics = await db.Topic.findAll()
+  let { title, description, topic } = req.body;
+  const validatorErrors = validationResult(req);
+  const question = await db.Question.findByPk(req.params.id)
+  if (validatorErrors.isEmpty()) {
+    const newQuestion = await question.update({
+      title,
+      description,
+      topicId: topic,
+      userId: user.id
+    });
+    await newQuestion.save()
+    res.redirect('/questions')
+  } else {
+    const errors = validatorErrors.array().map((error) => error.msg);
+    res.render('add-question', { errors, topics, csrfToken: req.csrfToken() })
+  }
+}))
 module.exports = router;
