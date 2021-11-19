66a0533ff5d6f843d29db39df2c54323a82ffb18
Your Name <email@example.com>
2021-11-19 17:38:22 -0500

    logo add and logout buttons styled

--- routes/search.js
+++ routes/search.js
@@ -2,11 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { asyncHandler } = require('./utils')
 const db = require('../db/models')
-const { Question } = require('../db/models');
 const { Op } = require('sequelize');
-//Helper function from the search data file
-// const searchRepo = require('../search-data/searchdata');
-router.post('/', async (req, res) => {
+
+// Helper function from the search data file
+router.post('/', asyncHandler(async (req, res) => {
     const user = await db.User.findByPk(req.session.auth.userId)
     const { value } = req.body
     const questions = await db.Question.findAll({
@@ -23,7 +22,19 @@ router.post('/', async (req, res) => {
     });
     const topics = await db.Topic.findAll()
     res.render('questions', { user, questions, topics });
-})
+}))
+
+router.get('/questions/:id', asyncHandler(async (req, res, next) => {
+    const question = await db.Question.findByPk(req.params.id, {
+      include: [{
+        model: db.Answer
+      },
+      { model: db.Topic }
+      ]
+    })
+    const user = await db.User.findByPk(req.session.auth.userId)
+    res.render('single-question', { question, user })
+  }))
 
 
 module.exports = router;
