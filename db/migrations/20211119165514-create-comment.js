15fa024d83ced74dd425c231e65c67f65073d3b6
Jared B <jburnt17@gmail.com>
2021-11-19 12:04:05 -0500

    added comments table and associations

---
+++ db/migrations/20211119165514-create-comment.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      answerId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {model: 'Answers'}
+      },
+      questionId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {model: 'Questions'}
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Comments');
+  }
+};
